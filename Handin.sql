965176715409e6463421b758f45c03f79160b853
Andrew Juby <jubydoo@gmail.com>
2018-07-19 13:30:41 -0500

    Section 2 complete

--- Handin.sql
+++ Handin.sql
@@ -14,3 +14,18 @@ Insert into EMPLOYEE (EMPLOYEEID, LASTNAME, FIRSTNAME) values (9, 'Flintstone', 
 Insert into EMPLOYEE (EMPLOYEEID, LASTNAME, FIRSTNAME) values (10, 'Jetson', 'George');
 Insert into CUSTOMER (CUSTOMERID, LASTNAME, FIRSTNAME, EMAIL) values (60, 'Wiggins', 'Andrew', 'ender@fleet.gov');
 Insert into CUSTOMER (CUSTOMERID, LASTNAME, FIRSTNAME, EMAIL) values (61, 'Potter', 'Harry', 'hpotter@hogwarts.edu');
+
+--2.4
+Update customer set firstname = 'Robert', lastname = 'Walter' where customerid = 32;
+Update artist set name = 'CCR' where artistid = 76;
+
+--2.5
+Select * from invoice where billingaddress like 'T%';
+
+--2.6
+Select * from invoice where total between 15 and 50;
+
+--2.7
+Delete from invoiceline where exists (select * from invoice where invoiceline.invoiceid = invoice.invoiceid and customerid = 32);
+Delete from invoice where customerid = 32;
+Delete from customer where customerid = 32;
